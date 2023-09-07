d779d5bc2494ffd87b77dd1d5d3506376363e91b
Levi Muniz <levi.muniz17@gmail.com>
2023-09-07 16:43:15 -0600

    deps: Bump target python version to 3.9 >, <= 3.11

--- scripts/test.bat
+++ scripts/test.bat
@@ -1,3 +1,3 @@
-docker run --rm -v %cd%:/usr/src/app -w /usr/src/app python:3.9-alpine sh -c ^
+docker run --rm -v %cd%:/usr/src/app -w /usr/src/app python:3.10-alpine sh -c ^
 "pip install tox black pylint -rrequirements.txt -rtest-requirements.txt && ^
-(black . ; pylint app/ ; tox -e py39)"
+(black . ; pylint app/ ; tox -e py310)"
